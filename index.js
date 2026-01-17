3662184ee540bf87d477d5f231cd09a85fb4096e
Gabriel Parry <gparry1@kent.edu>
2026-01-16 21:30:43 -0500

    the perfect breakout game

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ let ballY = 150;
 let ballDX = 1;
 let ballDY = 1;
 
+let brickInit = false;
+let bricks = [];
+
+
+
 window.onload = function() {
     c = document.getElementById("myCanvas");
     ctx = c.getContext("2d");
@@ -43,6 +48,9 @@ function drawLoop(){
         case "GAMEOVER":
             drawGameOver();
             break;
+        case "WIN":
+            drawGameWin();
+            break;
 
     }
 
@@ -59,12 +67,38 @@ function drawMainMenu(){
 }
 
 function drawGame(){
+
+    if(!brickInit) initBricks();
     
     ctx.beginPath();
     ctx.rect(mouseX - 25, 275, 50, 15);
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+
+    let anyBrickLeft = false;
+    bricks.forEach(brick => {
+        
+        if(brick.visible){
+            anyBrickLeft = true;
+
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.width, brick.height);
+            ctx.fillStyle = "lightblue";
+            ctx.fill();
+            ctx.closePath();
+
+             if(ballX > brick.x && ballX < brick.x + brick.width && ballY < brick.y + brick.height){
+                brick.visible = 0;
+                ballDY *= -1;
+            }
+        }
+    });
+
+    if(!anyBrickLeft){
+        gameState = "WIN";
+        gameReset();
+    }
 
     ballX += ballDX;
     ballY += ballDY;
@@ -81,6 +115,12 @@ function drawGame(){
 
     // check for hit platform
     if(ballX > mouseX - 25 && ballX < mouseX + 25 && ballY > 270){
+        if(Math.random() > .5){
+            ballDX = 0.5 + Math.random() * 2;
+        } else {
+            ballDX = -0.5 - Math.random() * 2;
+        }
+        ballDY = 0.5 + Math.random() * 2;
         ballDY *= -1;
     }
 
@@ -89,9 +129,11 @@ function drawGame(){
         ballDY *= -1;
     }
 
+    ctx.beginPath();
     ctx.arc(ballX, ballY, 5, 0, 2 * Math.PI);
     ctx.fillStyle = 'black';
     ctx.fill();
+    ctx.closePath();
 
 }
 
@@ -101,9 +143,26 @@ function drawGameOver(){
     ctx.fillText("You lost, press q to restart", 10, 50);
 }
 
+function drawGameWin(){
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("You win! Nice! Press q to restart", 10, 50);
+}
+
 function gameReset(){
     ballX = 50;
     ballY = 150;
     ballDX = 1;
     ballDY = 1;
+    bricks = [];
+    brickInit = false;
+}
+
+function initBricks(){
+    for(let i = 0; i < 10; i++){
+        for(let j = 0; j < 3; j++){
+            bricks.push({x: i * 45 + 10, y: 10 + (j * 20), width: 40, height: 15, visible: 1});
+        }
+    }
+    brickInit = true;
 }
