5b10ea63f70c22cac76607f49f10fb90eec9c8b1
Gabriel Parry <gparry1@kent.edu>
2026-01-16 20:47:37 -0500

    added ball

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let mouseX = 50;
 let c, ctx;
 let gameState = "MAINMENU";
+let mouseX = 50;
+
+let ballX = 50;
+let ballY = 150;
+let ballDX = 1;
+let ballDY = 1;
 
 window.onload = function() {
     c = document.getElementById("myCanvas");
@@ -20,6 +25,7 @@ window.addEventListener('keydown', function(event) {
     }
     if(event.key === 'q' || event.key === 'Q'){
         gameState = "MAINMENU";
+        gameReset();
     }
 });
 
@@ -33,6 +39,9 @@ function drawLoop(){
             break;
         case "GAME":
             drawGame();
+            break;
+        case "GAMEOVER":
+            drawGameOver();
             break;
 
     }
@@ -56,4 +65,45 @@ function drawGame(){
     ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+
+    ballX += ballDX;
+    ballY += ballDY;
+
+    // check for hit bottom
+    if(ballY > 320){
+        gameState = "GAMEOVER";
+    }
+
+    // check for hit sides
+    if(ballX > 475 || ballX < 5){
+        ballDX *= -1;
+    }
+
+    // check for hit platform
+    if(ballX > mouseX - 25 && ballX < mouseX + 25 && ballY > 270){
+        ballDY *= -1;
+    }
+
+    // check for hit top
+    if(ballY < 5){
+        ballDY *= -1;
+    }
+
+    ctx.arc(ballX, ballY, 5, 0, 2 * Math.PI);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+
+}
+
+function drawGameOver(){
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("You lost, press q to restart", 10, 50);
+}
+
+function gameReset(){
+    ballX = 50;
+    ballY = 150;
+    ballDX = 1;
+    ballDY = 1;
 }
